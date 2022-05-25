d4df83e865fb2f086d71a8acfe7379c18fac30f6
2001321060 <2001321060@9f8082a7-18b2-4f14-acb1-ac6f4c35b7b8>
2022-05-25 13:35:59 +0000

    added some code about pencil and eraser so far

--- Paint/Paint/Form1.cs
+++ Paint/Paint/Form1.cs
@@ -12,12 +12,67 @@ namespace Paint
 {
     public partial class PaintApplication : Form
     {
+        Bitmap bm;
+        Graphics graphics;
+        bool paint = false;
+        Point px, py;
+        Pen pen = new Pen(Color.Black, 1);
+        Pen erase = new Pen(Color.White, 10);
+        int index;
         public PaintApplication()
         {
             InitializeComponent();
+            this.Width = 900;
+            this.Height = 700;
+            bm = new Bitmap(pic.Width, pic.Height);
+            graphics = Graphics.FromImage(bm);
+            graphics.Clear (Color.White);
+            pic.Image = bm;
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            py = e.Location;
+
+        }
+
+        private void pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                if(index == 1)
+                {
+                    px = e.Location;
+                    graphics.DrawLine(pen, px, py);
+                    py = px;
+                } else if (index == 2) { 
+                    px = e.Location;
+                    graphics.DrawLine(pen, px, py);
+                    py = px;
+                }
+            }
+            pic.Refresh();
+        }
+
+        private void pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+
+        }
+
+        private void pencilButton_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+
+        private void eraserButton_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+
+        private void PaintApplication_Load(object sender, EventArgs e)
         {
 
         }
